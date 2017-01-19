af59ae795a52e515e4ab5076ab3c0dc00ac12e47
Ryan Clarke <ryan@ryanclarke.net>
2017-01-19 09:10:07 -0500

    Create index.html and app.js files

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var wrapper = document.getElementById("wrapper");
+    var text = document.createTextNode("This is it.")
+    wrapper.appendChild(text);
+});
