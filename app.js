589459d9f088c95dde27f431444b095cc69155f2
Ryan Clarke <ryan@ryanclarke.net>
2017-01-26 16:59:19 -0500

    Add purecss; Enable adding new todo items

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var wrapper = document.getElementById("wrapper");
-    var text = document.createTextNode("This is it.")
-    wrapper.appendChild(text);
-});
+var addItemToTodoList = function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        var input = document.getElementById("todo-add-item-input");
+        newTodoItem(input.value);
+        input.value = "";
+    }
+}
+
+var newTodoItem = function(text) {
+    var list = document.getElementById("todo-list");
+
+    var grid = document.createElement("div");
+    grid.classList.add("pure-u-1");
+
+    var todoItem = document.createElement("div");
+    todoItem.classList.add("todo-list-item");
+    todoItem.classList.add("pure-form");
+    grid.appendChild(todoItem);
+
+    var label = document.createElement("label");
+    var id = `${new Date().valueOf()}${Math.random()}`;
+    label.setAttribute("for", id);
+    label.classList.add("pure-checkbox");
+    label.innerHTML = `<input id="${id}" type="checkbox"> ${text}`;
+    todoItem.appendChild(label);
+
+    list.appendChild(grid);
+};
+
+var domContentLoaded = function() {
+    newTodoItem("thing 1");
+    newTodoItem("thing 2");
+    
+    document.getElementById("todo-add-item-input").addEventListener("keyup", addItemToTodoList);
+};
+
+document.addEventListener("DOMContentLoaded", domContentLoaded, true);
