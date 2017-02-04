5f6bb6ceaef51a1870c16e5a7fbbdb54ebf19e85
Ryan Clarke <ryan@ryanclarke.net>
2017-02-03 20:42:04 -0500

    Move code to task object with create function

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
+var task = {
+    create: function(text) {
+        var taskId = `${new Date().valueOf()}${Math.random()}`;
+
+        var list = document.getElementById("todo-list");
+
+        var task = document.createElement("div");
+        task.classList.add("todo-list-task");
+        task.classList.add("pure-form-aligned");
+        task.classList.add("pure-u-1");
+
+        var label = document.createElement("label");
+        label.setAttribute("for", taskId);
+        label.classList.add("pure-checkbox");
+        task.appendChild(label);
+
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = taskId;
+        checkbox.onchange = onTaskChecked(checkbox, list, task);
+        label.appendChild(checkbox);
+
+        var text = document.createTextNode(` ${text}`);
+        label.appendChild(text);
+
+        list.appendChild(task);
+    }
+}
+
 var addTaskToTodoList = function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
         var input = document.getElementById("todo-add-task-input");
-        newTodoItem(input.value);
+        task.create(input.value);
         input.value = "";
     }
 }
 
-var newTodoItem = function(text) {
-    var list = document.getElementById("todo-list");
-
-    var task = document.createElement("div");
-    task.classList.add("todo-list-task");
-    task.classList.add("pure-form");
-    task.classList.add("pure-u-1");
-
-    var label = document.createElement("label");
-    var id = `${new Date().valueOf()}${Math.random()}`;
-    label.setAttribute("for", id);
-    label.classList.add("pure-checkbox");
-
-    var input = document.createElement("input");
-    input.type = "checkbox";
-    input.id = id;
-    input.onchange = function() {
+var onTaskChecked = function(input, list, task) {
+    var event = function() {
         if (input.checked) {
             list.removeChild(task);
         }
-    };
-    task.appendChild(input);
-
-    var text = document.createTextNode(" " + text);
-    task.appendChild(text);
-
-    list.appendChild(task);
-};
+    }
+    return event;
+}
 
 var domContentLoaded = function() {
-    newTodoItem("thing 1");
-    newTodoItem("thing 2");
+    task.create("thing 1");
+    task.create("thing 2");
     
     document.getElementById("todo-add-task-input").addEventListener("keyup", addTaskToTodoList);
-};
+}
 
 document.addEventListener("DOMContentLoaded", domContentLoaded, true);
