f007ebb3e105bb269e9174a3c849de04b30cbbf1
Ryan Clarke <ryan@ryanclarke.net>
2017-02-09 08:40:21 -0500

    Add button to increase task priority

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
-var task = {
-    template: {
+var todoList = {
+    taskTemplate: {
         original: document.querySelector("#task-template").content.firstElementChild,
         clone() {
-            return document.importNode(this.original, true)
+            return document.importNode(this.original, true);
         }
     },
-    addToList(text) {
+    addTask(text) {
         var taskId = `${new Date().valueOf()}${Math.random()}`;
 
         var list = document.getElementById("todo-list");
 
-        var task = this.template.clone();
+        var task = this.taskTemplate.clone();
         var label = task.firstElementChild;
         var checkbox = label.firstElementChild;
+        var prioritize = task.querySelector(".task-priority-increase");
         var text = document.createTextNode(` ${text}`);
 
         label.setAttribute("for", taskId);
         checkbox.id = taskId;
-        checkbox.onchange = onTaskChecked(checkbox, list, task);
+        checkbox.onchange = todoTask.completed(checkbox, list, task);
+
+        prioritize.onclick = todoTask.prioritize(list, task);
 
         label.appendChild(text);
 
         list.appendChild(task);
-    }
-}
-
-var addTaskToTodoList = function(event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-        var input = document.getElementById("todo-add-task-input");
-        task.addToList(input.value);
-        input.value = "";
-    }
-}
-
-var onTaskChecked = function(input, list, task) {
-    var event = function() {
-        if (input.checked) {
-            list.removeChild(task);
+    },
+    onEnterPressed(event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            var input = document.getElementById("todo-add-task-input");
+            todoList.addTask(input.value);
+            input.value = "";
         }
     }
-    return event;
+}
+
+var todoTask = {
+    completed(input, list, task) {
+        var event = function() {
+            if (input.checked) {
+                list.removeChild(task);
+            }
+        }
+        return event;
+    },
+    prioritize(list, task) {
+        var event = function() {
+            list.insertBefore(task, task.previousSibling);
+        }
+        return event;
+    }
 }
 
 var domContentLoaded = function() {
-    task.addToList("thing 1");
-    task.addToList("thing 2");
-    
-    document.getElementById("todo-add-task-input").addEventListener("keyup", addTaskToTodoList);
+    todoList.addTask("thing 1");
+    todoList.addTask("thing 2");
+
+    document.getElementById("todo-add-task-input").addEventListener("keyup", todoList.onEnterPressed);
 }
 
 document.addEventListener("DOMContentLoaded", domContentLoaded, true);
