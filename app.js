cb35e259de8b46f463afb6302685683cee9d0740
Ryan Clarke <ryan@ryanclarke.net>
2017-02-17 05:07:13 -0500

    Move top-level DOM queries to app object

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var app = {
+    todoList: document.getElementById("todo-list"),
+    doneList: document.getElementById("done-list"),
+    newTaskInput: document.getElementById("todo-add-task-input")
+}
+
 var todoList = {
     taskTemplate: {
         original: document.querySelector("#task-template").content.firstElementChild,
@@ -7,8 +13,6 @@ var todoList = {
     },
     addTask(text) {
         var taskId = `${new Date().valueOf()}${Math.random()}`;
-
-        var list = document.getElementById("todo-list");
 
         var task = this.taskTemplate.clone();
         var label = task.firstElementChild;
@@ -21,19 +25,18 @@ var todoList = {
         checkbox.id = taskId;
         checkbox.onchange = todoTask.complete(checkbox, task);
 
-        prioritize.onclick = todoTask.prioritize(list, task);
+        prioritize.onclick = todoTask.prioritize(task);
         discard.onclick = todoTask.discard(task);
 
         label.appendChild(text);
 
-        list.appendChild(task);
+        app.todoList.appendChild(task);
     },
     onEnterPressed(event) {
         event.preventDefault();
         if (event.keyCode == 13) {
-            var input = document.getElementById("todo-add-task-input");
-            todoList.addTask(input.value);
-            input.value = "";
+            todoList.addTask(app.newTaskInput.value);
+            app.newTaskInput.value = "";
         }
     }
 }
@@ -42,10 +45,8 @@ var todoTask = {
     complete(input, task) {
         var event = function() {
             if (input.checked) {
-                var todoList = document.getElementById("todo-list");
-                todoList.removeChild(task);
-                var doneList = document.getElementById("done-list");
-                doneList.insertBefore(task, doneList.firstChild);
+                app.todoList.removeChild(task);
+                app.doneList.insertBefore(task, app.doneList.firstChild);
                 task.onchange = todoTask.uncomplete(input, task);
             }
         }
@@ -54,18 +55,16 @@ var todoTask = {
     uncomplete(input, task) {
         var event = function() {
             if (!input.checked) {
-                var doneList = document.getElementById("done-list");
-                doneList.removeChild(task);
-                var todoList = document.getElementById("todo-list");
-                todoList.insertBefore(task, todoList.firstChild);
+                app.doneList.removeChild(task);
+                app.todoList.insertBefore(task, app.todoList.firstChild);
                 task.onchange = todoTask.complete(input, task);
             }
         }
         return event;
     },
-    prioritize(list, task) {
+    prioritize(task) {
         var event = function() {
-            list.insertBefore(task, task.previousSibling);
+            task.parentNode.insertBefore(task, task.previousSibling);
         }
         return event;
     },
@@ -81,9 +80,8 @@ var domContentLoaded = function() {
     todoList.addTask("thing 1");
     todoList.addTask("thing 2");
 
-    var addNewTask = document.getElementById("todo-add-task-input");
-    addNewTask.focus();
-    addNewTask.addEventListener("keyup", todoList.onEnterPressed);
+    app.newTaskInput.focus();
+    app.newTaskInput.addEventListener("keyup", todoList.onEnterPressed);
 }
 
 document.addEventListener("DOMContentLoaded", domContentLoaded, true);
